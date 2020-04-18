125fdb5ba20441e730f6f213e05ad0a3b57b6550
juanchete <juanchete@gmail.com>
2020-04-18 18:48:21 +0200

    Initial commit

--- app.py
+++ app.py
@@ -1,29 +1,16 @@
 import os
-from flask import Flask
-from models import setup_db
+from flask import Flask, request, abort, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 
 def create_app(test_config=None):
+  # create and configure the app
+  app = Flask(__name__)
+  CORS(app)
 
-    app = Flask(__name__)
-    setup_db(app)
-    CORS(app)
+  return app
 
-    @app.route('/')
-    def get_greeting():
-        excited = os.environ['EXCITED']
-        greeting = "Hello" 
-        if excited == 'true': greeting = greeting + "!!!!!"
-        return greeting
-
-    @app.route('/coolkids')
-    def be_cool():
-        return "Be cool, man, be coooool! You're almost a FSND grad!"
-
-    return app
-
-app = create_app()
+APP = create_app()
 
 if __name__ == '__main__':
-    app.run()
+    APP.run(host='0.0.0.0', port=8080, debug=True)
