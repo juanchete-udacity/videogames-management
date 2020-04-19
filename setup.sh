bae3d79d2a2706563e327c2dd157b93eb27a4b5f
juanchete <juanchete@gmail.com>
2020-04-19 21:13:36 +0200

    heroku variables

--- setup.sh
+++ setup.sh
@@ -1,3 +1,7 @@
-FLASK_APP=app.py
-FLASK_ENV=development
-VIDEOGAMES_PER_PAGE=10
+export FLASK_ENV='development'
+export FLASK_APP='app.py'
+export VIDEOGAMES_PER_PAGE=5
+export AUTH0_DOMAIN='dev-juanchete.eu.auth0.com'
+export API_AUDIENCE='videoGamesAPI'
+export CLIENT_ID='VpsuaVE5l6k5XnV0E2fd75aZHzVv676V'
+export REDIRECT_URI='https://videogames-juanchete-udacity.herokuapp.com/login-results'
