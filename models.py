8a5179c6a2ebb8c86f28828eb31ef85a91d3f7ba
juanchete <juanchete@gmail.com>
2020-04-21 17:43:17 +0200

    Pep8 on models.py

--- models.py
+++ models.py
@@ -19,7 +19,7 @@ def setup_db(app, database_path=database_path, test_mode=False):
         app.config["SQLALCHEMY_DATABASE_URI"] = database_path+'_test'
     else:
         app.config["SQLALCHEMY_DATABASE_URI"] = database_path
-        
+
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.app = app
     db.init_app(app)
